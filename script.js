b5648be4849297e32c8643b177586dd143a761a9
akp98 <165936122+akp98@users.noreply.github.com>
2025-01-23 23:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission (page reload)
+    event.preventDefault();
+    
+    // Get the values of first name and last name from the form
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
 
+    // Display the full name in an alert
+    alert(firstName + " " + lastName);
 }
